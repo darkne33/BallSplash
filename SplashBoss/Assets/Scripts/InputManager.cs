9526e9c477055e2cfc4206eee6f3a78f616d5aca
darkne33 <myworld48@mail.ru>
2020-03-20 10:20:01 +0300

    Project
    In git

---
+++ SplashBoss/Assets/Scripts/InputManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public InputData inputData;
+
+    private void Update()
+    {
+        WriteInputData();
+    }
+
+    public void WriteInputData()
+    {
+        inputData.isPressed = Input.GetMouseButtonDown(0);
+        inputData.isHeld = Input.GetMouseButton(0);
+        inputData.isReleased = Input.GetMouseButtonUp(0);
+    }
+}
